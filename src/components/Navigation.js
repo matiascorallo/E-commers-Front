b1db2fbbe62a31a55aa6a0c87020bce254260282
Matias Corallo <matiascorallodev@gmail.com>
2023-02-16 19:00:39 -0300

    segundo commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Navbar, Button, Container, Nav, NavDropdown} from 'react-bootstrap';    
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import './navigation.css'
 
 
 function navigation() {
@@ -7,12 +10,13 @@ function navigation() {
 
         <Navbar bg="light" expand="lg">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <LinkContainer to="/">
+            <Navbar.Brand >E-commers</Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+              <Nav className="ms-auto">
+                <Nav.Link href="/login">Login</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
+import categories from '../categories'
+import './home.css'
 
 function Home() {
   return (
-    <div>Home page</div>
+    <div>
+
+      <img src="https://res.cloudinary.com/learn-code-10/image/upload/v1653947013/yqajnhqf7usk56zkwqi5.png" className='home-banner' />
+
+      <div className='featured-products'>
+        <h2>Last products</h2>
+        <div>
+          <Link to="/category/all" style={{ textAling: 'right', display: 'block', textDecoration: 'none' }}>See more {">>"}
+          </Link>
+        </div>
+      </div>
+      <div className="sale__banner--container mt-4">
+        <img src="https://res.cloudinary.com/learn-code-10/image/upload/v1654093280/xkia6f13xxlk5xvvb5ed.png" />
+      </div>
+      <div className="recent-products-container container mt-4">
+        <h2>Categories</h2>
+        <Row>
+          {categories.map((category) => (
+            <LinkContainer to={`/category/${category.name.toLocaleLowerCase()}`}>
+              <Col md={4}>
+                <div style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${category.img})`, gap: "10px" }} className="category-tile">
+                  {category.name}
+                </div>
+              </Col>
+            </LinkContainer>
+          ))}
+        </Row>
+      </div>
+    </div>
   )
 }
 
